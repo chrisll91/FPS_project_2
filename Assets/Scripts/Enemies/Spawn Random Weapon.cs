09ef18f93945ed9cedd7982801070f5b0d729f9a
chrisll91 <chris_ll91@hotmail.com>
2026-05-21 22:58:23 +0200

    random pick up script

--- Assets/Scripts/Enemies/Spawn Random Weapon.cs
+++ Assets/Scripts/Enemies/Spawn Random Weapon.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SpawnRandomWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] weaponPrefabs;
-    //[SerializeField] float respawnDelay = 1f;
+    
 
     private void Start()
     {
