d87dc3d2cd280d0c1fa9a1810bd710f4e4a33dea
chrisll91 <chris_ll91@hotmail.com>
2026-06-02 18:20:29 +0200

    Update PauseMenu.cs

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -2,43 +2,5 @@ using StarterAssets;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
-{
-    [SerializeField] GameObject pauseScreen;
-
-    private StarterAssetsInputs starterAssetsInputs;
-
-    private bool isPaused = false;
-
-    private void Awake()
-    {
-        starterAssetsInputs =
-            FindFirstObjectByType<StarterAssetsInputs>();
-
-        pauseScreen.SetActive(false);
-    }
-
-    private void Update()
-    {
-        if (starterAssetsInputs.Pause)
-        {
-            TogglePause();
-
-            starterAssetsInputs.Pause = false;
-        }
-    }
-
-    public void TogglePause()
-    {
-        isPaused = !isPaused;
-
-        pauseScreen.SetActive(isPaused);
-
-        Time.timeScale = isPaused ? 0f : 1f;
-
-        Cursor.lockState = isPaused
-            ? CursorLockMode.None
-            : CursorLockMode.Locked;
-
-        Debug.Log("Pause toggled");
-    }
+{      
 }
